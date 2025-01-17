b008c0d76b3e245dd82a5c7df90a405f427c8aba
Branko <home.branko@gmail.com>
2025-01-17 03:19:02 +0100

    open book picture added

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,6 +3,7 @@ function Header() {
     <header className="flex bg-slate-500">
       <img src="/pearls3.png" alt="pearls" className="w-20" />
       <h1 className="m-4 text-6xl font-bold text-orange-300">Dictionary</h1>
+      <img src="/open_book.png" alt="book" className="h-48" />
     </header>
   );
 }
